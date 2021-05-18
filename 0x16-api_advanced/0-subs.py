839241ca8020a22b9329fec07930770311e2f1f2
Ethan-23 <emanroberts@gmail.com>
2021-05-18 18:32:33 +0000

    added task 0

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Gets number of subscribers a sub reddit has"""
+import requests
+
+
+def number_of_subscribers(subreddit):
+    url = "https://www.reddit.com/"
+    headers = requests.utils.default_headers()
+    headers.update(
+        {
+            'User-Agent': 'My User Agent 1.0',
+        }
+    )
+    subs = requests.get(url + "r/" + subreddit + "/about.json",
+                        headers=headers)
+    subs_dict = subs.json()["data"]["subscribers"]
+    return(subs_dict)
